7d5fb6fe61e16553be9d68f73f209b25af22bb70
Ramesh Nair <ram@hiddentao.com>
2018-08-29 14:00:48 +0100

    Remove ENS stuff

--- test/encryption.js
+++ test/encryption.js
@@ -3,7 +3,6 @@ const Conference = artifacts.require("Conference.sol");
 let deposit, conference;
 let twitterOne = '@twitter1';
 var crypto = require('crypto');
-var cryptoBrowserify = require('crypto-browserify');
 var fs = require('fs');
 
 const getTransaction = function(type, transactionHash){
@@ -28,7 +27,7 @@ contract('Encryption', function(accounts) {
       var message = "マコト";
       conference = await Conference.new('', 0, 0, 10, publicKey, '0');
       var publicKeyFromContract = await conference.encryption.call();
-      var encrypted = cryptoBrowserify.publicEncrypt(publicKeyFromContract, new Buffer(message, 'utf-8'));
+      var encrypted = crypto.publicEncrypt(publicKeyFromContract, new Buffer(message, 'utf-8'));
 
       console.log(getTransaction('create   ', conference.transactionHash));
       deposit = (await conference.deposit.call()).toNumber();
