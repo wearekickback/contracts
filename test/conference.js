23951ebf8a07fe84f59518730267ed1cdf423230
Ramesh Nair <ram@hiddentao.com>
2018-10-03 09:56:55 +0100

    finalize() cannot be called by admins

--- test/conference.js
+++ test/conference.js
@@ -243,12 +243,9 @@ contract('Conference', function(accounts) {
       await conference.attended().should.eventually.eq(1)
     })
 
-    it('can be called by admin', async function(){
+    it('cannot be called by admin', async function(){
       await conference.grant([admin], {from:owner});
-      await conference.finalize([1], {from:admin});
-
-      await conference.isAttended(non_owner).should.eventually.eq(true)
-      await conference.attended().should.eventually.eq(1)
+      await conference.finalize([1], {from:admin}).should.be.rejected;
     })
 
     it('cannot be called by non owner', async function(){
