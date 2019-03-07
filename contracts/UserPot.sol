86bd4fc5601e0a0d22f99428336557249a5b8418
Ramesh Nair <ram@hiddentao.com>
2019-03-07 14:54:34 +0000

    improvements to user pot logic

--- contracts/UserPot.sol
+++ contracts/UserPot.sol
@@ -8,9 +8,16 @@ import "./UserPotInterface.sol";
 
 contract UserPot is Upgradeable, RBACWithAdmin, UserPotInterface {
   bytes32 public constant STORAGE_KEY_EVENTS = keccak256("events");
-  bytes32 public constant STORAGE_KEY_LEFTOVER = keccak256("leftover");
 
-  bytes4 INTERFACE_ID = bytes4(keccak256('KickbackUserPot'));
+  bytes32 public constant STORAGE_KEY_BALANCE = keccak256("balance");
+
+  bytes4 public constant INTERFACE_ID = bytes4(keccak256('KickbackUserPot'));
+
+  // can only really do fixed-size arrays in memory, plus loops can be expensive,
+  // so we limit the max here. But we should calculate the gas cost of the
+  // various methods (especially this one) and then work out a reasonable
+  // limit based on that. That limit will then need to be enforced app-side as well
+  uint public constant MAX_ACTIVE_EVENTS_PER_USER = 25;
 
   StorageInterface dataStore;
 
@@ -19,18 +26,18 @@ contract UserPot is Upgradeable, RBACWithAdmin, UserPotInterface {
   }
 
   function deposit(address _user) external payable {
-    EventInterface _event = EventInterface(msg.sender);
-    uint256 _deposit = _event.getDeposit(_user);
-    uint256 bal = calculatePayout(_user);
+    EventInterface event = EventInterface(msg.sender);
+    uint256 deposit = event.getDeposit(_user);
+    uint256 bal = _getUserTotalPayout(_user);
     bal += msg.value;
-    require(bal >= _deposit, 'you need to pay more to register for event');
-    _updateUserData(_user, msg.sender, bal - _deposit);
+    require(bal >= deposit, 'please pay more to register for this event');
+    _updateUserData(_user, msg.sender, bal - deposit);
   }
 
   function withdraw() external {
-    uint256 bal = calculatePayout(msg.sender);
-    msg.sender.transfer(bal);
+    uint256 bal = _calculatePayout(msg.sender);
     _updateUserData(msg.sender, address(0), 0);
+    msg.sender.transfer(bal);
   }
 
   /* Update the data associated with this user in the data storage contract.
@@ -45,39 +52,47 @@ contract UserPot is Upgradeable, RBACWithAdmin, UserPotInterface {
   function _updateUserData(address _user, address _newEvent, uint256 _newBalance) internal {
     address[] memory events = dataStore.getAddressList(_user, STORAGE_KEY_EVENTS);
 
-    // can only really do fixed-size arrays in memory, so we limit to 10. But we should calculate the gas cost of
-    // the various methods (especially this one) and then work out a reasonable limit based on that. That limit will
-    // then need to be enforced app-side as well
-    address[] memory newEvents = new address[](10);
+    address[] memory newEvents = new address[](MAX_ACTIVE_EVENTS_PER_USER);
 
     uint256 newEventsLen = 0;
 
     for (uint256 i = 0; i < events.length; i += 1) {
       EventInterface e = EventInterface(events[i]);
-      // remove ended events
+      // only count non-ended events, ignore ended ones
       if (!e.hasEnded()) {
         newEvents[newEventsLen] = events[i];
         newEventsLen++;
       }
     }
+
+    // at this point we cannot have total event capacity
+    require(newEventsLen < MAX_ACTIVE_EVENTS_PER_USER, 'attending too many events')
+
+    // add a new event to this list?
     if (_newEvent != address(0)) {
       newEvents[newEventsLen] = _newEvent;
       newEventsLen++;
     }
-    dataStore.setUint(_user, STORAGE_KEY_LEFTOVER, _newBalance);
+
+    dataStore.setUint(_user, STORAGE_KEY_BALANCE, _newBalance);
     dataStore.setAddressList(_user, STORAGE_KEY_EVENTS, newEvents, newEventsLen);
   }
 
-  function calculatePayout(address _user) public view returns (uint256) {
-    uint256 bal = dataStore.getUint(_user, STORAGE_KEY_LEFTOVER);
+  function _getUserTotalPayout(address _user) internal view return (uint256) {
+    uint256 bal = dataStore.getUint(_user, STORAGE_KEY_BALANCE);
     address[] memory events = dataStore.getAddressList(_user, STORAGE_KEY_EVENTS);
     for (uint256 i = 0; i < events.length; i += 1) {
         EventInterface e = EventInterface(events[i]);
+        // only count ended events
         if (e.hasEnded()) {
             bal += e.getPayout(_user);
         }
     }
     return bal;
+  }
+
+  function calculatePayout(address _user) public view returns (uint256) {
+    return _getUserTotalPayout(user)
   }
 
   function calculateDeposit(address _user) public view returns (uint256) {
--- contracts/UserPotInterface.sol
+++ contracts/UserPotInterface.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.4;
 
 interface UserPotInterface {
-  function deposit(address _event, address _user, uint256 _deposit) external payable;
+  function deposit(address _user) external payable;
   function withdraw() external;
   function calculatePayout(address _user) external view returns (uint256);
   function calculateDeposit(address _user) external view returns (uint256);
