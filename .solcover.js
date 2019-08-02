2783cd908434dd86afac48e7c152b172a8d1c17f
Makoto <inouemak@googlemail.com>
2019-08-03 00:01:42 +0100

    Increase default balance of sol coverage

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,6 @@
 module.exports = {
   accounts: 350,
+  testrpcOptions: '--defaultBalanceEther 2000000',
   testCommand: '../node_modules/.bin/babel-node ../node_modules/.bin/truffle test --network coverage test/conference.js test/group_admin.js test/deployer.js',
   skipFiles: ['zeppelin/lifecycle/Destructible.sol','zeppelin/ownership/Ownable.sol']
 };
