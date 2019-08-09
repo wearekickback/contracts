04bdda1b9d7b9d7490220be4e2c730de52cc51c2
Makoto <inouemak@googlemail.com>
2019-08-09 16:23:27 +0100

    Fix testrpcOptions

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,7 @@
 module.exports = {
   accounts: 350,
-  norpc: true,
-  testrpcOptions: '--defaultBalanceEther 2000000 --gasLimit 0xfffffffffff',
-  testCommand: '../node_modules/.bin/babel-node ../node_modules/.bin/truffle test --network coverage test/group_admin.js',
+  port: 8555,
+  testrpcOptions: '--port 8555 --accounts 350 --defaultBalanceEther 2000000',
+  testCommand: '../node_modules/.bin/babel-node ../node_modules/.bin/truffle test --network coverage test/conference.js test/erc20_conference.js test/group_admin.js test/deployer.js',
   skipFiles: ['zeppelin/lifecycle/Destructible.sol','zeppelin/ownership/Ownable.sol']
 };
