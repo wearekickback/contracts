2416617f1e2195eb099cae61d26745ccff24b7d8
Makoto Inoue <2630+makoto@users.noreply.github.com>
2020-06-26 15:16:29 +0100

    Grant admins

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@ const Deployer = artifacts.require("./Deployer.sol");
 const EthDeployer = artifacts.require("./EthDeployer.sol");
 const ERC20Deployer = artifacts.require("./ERC20Deployer.sol");
 const Token = artifacts.require("MyToken.sol");
+const path = require('path')
 
 const coolingPeriod = 1 * 60 * 60 * 24 * 7;
 // this is already required by truffle;
@@ -16,13 +17,22 @@ let name = ''; // empty name falls back to the contract default
 let deposit = 0; // 0 falls back to the contract default
 let limitOfParticipants = 0; // 0 falls back to the contract default
 let clearFee = 50; // 5%
+let admins
 const emptyAddress = '0x0000000000000000000000000000000000000000';
 // eg: truffle migrate --config '{"name":"CodeUp No..", "limitOfParticipants":15}'
 if (yargs.argv.config) {
   config = JSON.parse(yargs.argv.config);
 }
+const adminFile = './.admins.js'
 
 module.exports = function(deployer) {
+  if (fs.existsSync(adminFile)) {
+    admins = fs.readFileSync(adminFile, 'utf8').slice(0,-1).split('\n')
+    console.log({admins})
+  }else{
+    console.log(`No admin addreses set on ${adminFile}`)
+  }
+
   if (deployer.network == 'test' || deployer.network == 'coverage') return 'no need to deploy contract';
   if (config.name){
     name = config.name;
@@ -43,6 +53,10 @@ module.exports = function(deployer) {
     await deployer.deploy(Deployer, ethDeployer.address, erc20Deployer.address, clearFee);
     const mainDeployer = await Deployer.deployed();
     console.log([name, deposit,limitOfParticipants, coolingPeriod, clearFee].join(','));
+    if(admins){
+      await mainDeployer.grant(admins)
+    }
+    console.log('Admin granted to', admins)
     if(deployer.network == 'docker' || deployer.network == 'development'){
       console.log('deploying a party', {name, deposit,limitOfParticipants, coolingPeriod, emptyAddress})
       await mainDeployer.deploy(name, deposit,limitOfParticipants, coolingPeriod, emptyAddress);
