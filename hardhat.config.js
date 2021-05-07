a71976d58841390094e86c24268a1a070997f1bf
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-05-07 20:50:17 +0100

    Upgrade to node v14 and switch to hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,11 @@ require("@nomiclabs/hardhat-truffle5");
 module.exports = {
   networks: {
       hardhat: {
+        gasPrice:1,
+        accounts:{
+          count: 500,
+          accountsBalance: '2000000000000000000000000'
+        }      
       }
   },
   mocha: {
