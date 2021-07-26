e141817c7254b97e2d29e43137bdc65aa9e68a75
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-07-26 18:08:19 +0100

    Add Polygon  (#77)
    * Added an endpoint for Mumbai/Matic mainnet
    
    * v1.4.6

--- truffle-config.js
+++ truffle-config.js
@@ -50,6 +50,16 @@ module.exports = {
       gasPrice: 0x01, // <-- Use this low gas price
       network_id: 100
     },
+    polygon: {
+      provider: (num_addresses = 1) => new HDWalletProvider(mnemonic, `https://rpc-mainnet.maticvigil.com/v1/${infuraKey}`, 0, num_addresses),
+      gasPrice: 1000000000, // 1 gwei
+      network_id: 137
+    },
+    mumbai:{
+      provider: (num_addresses = 1) => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com/v1/${infuraKey}`, 0, num_addresses),
+      gasPrice: 1000000000, // 1 gwei
+      network_id: 80001
+    },
     mainnet: {
       // gas: 5000000,
       provider: (num_addresses = 1) => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${infuraKey}`, 0, num_addresses),
