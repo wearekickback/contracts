40674b929499c84f93cffa4376bba6f51a828670
niklr <r.niklaus@gmx.ch>
2021-08-09 16:30:22 +0200

    Remove ConferenceTicket contract

--- contracts/IConferenceTicket.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.11;
-
-interface IConferenceTicket {
-  function setBaseTokenURI(string calldata uri) external;
-  function setConferenceAddress(address _address) external;
-  function tokenURI(uint256 tokenId) external view returns (string memory);
-  function mint(address to, uint256 tokenId) external;
-}
